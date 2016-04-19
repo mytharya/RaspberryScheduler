6eac26f3b183a3b2db9eb664e5feeb4919775c21
Mihai Popa <popamihai84@gmail.com>
2016-04-19 22:03:34 +0200

    Updated files.

--- src/raspberryScheduler/RaspberryScheduler.java
+++ src/raspberryScheduler/RaspberryScheduler.java
@@ -1,18 +1,83 @@
 package raspberryScheduler;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class RaspberryScheduler {
 
 	private boolean isInitialized;
 	
+	private File settingsFile;
+	
+	private long settingsFileTimeStamp;
+	
+	private static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	
 	public RaspberryScheduler() {
 
+		boolean errorFound = false;
 		
-		this.isInitialized = readSettingsFile();
+		if(createFiles() == false)
+		{
+			errorFound = true;
+		}
+
+		if(errorFound == false)
+		{
+			errorFound = readSettingsFile();
+		}
+
+		if(errorFound == false)
+		{
+			this.isInitialized = true;
+		}
 	}
 	
 	private boolean readSettingsFile()
 	{
-		return true;
+		boolean errorFound = false;
+		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(settingsFile));
+			
+		    String line = br.readLine();
+		    while (line != null) {
+		    	
+		    	//TODO read schedule
+		    	
+		        line = br.readLine();
+		    }
+		    
+		} catch (Exception e) {
+			printMessage("Could not open settings file(" + RaspberrySchedulerSettings.getSettingsFileName() + ").");
+			errorFound = true;
+		} finally {
+			if(br != null)
+			{
+				try {
+					br.close();
+				} catch (Exception e2) {
+					errorFound = true;
+					printMessage("Could not close settings file(" + RaspberrySchedulerSettings.getSettingsFileName() + ").");
+				}
+			}
+		}
+		
+		if(errorFound == false)
+		{
+			settingsFileTimeStamp = settingsFile.lastModified();
+		}
+		
+		return errorFound;
 	}
 	
 	public boolean isInitialized()
@@ -22,6 +87,53 @@ public class RaspberryScheduler {
 	
 	public void start()
 	{
-		
+		while(true)
+		{
+			if(settingsFile.lastModified() != settingsFileTimeStamp)
+			{
+				if(readSettingsFile() == true)
+				{
+					printMessage("Setting file modified but could not be read.");
+					break;
+				}
+			}
+		}
 	}
+	
+	private boolean createFiles()
+	{
+		settingsFile = new File(RaspberrySchedulerSettings.getSettingsFileName());
+
+		if(!settingsFile.exists())
+		{
+			printMessage("Could not find settings file(" + RaspberrySchedulerSettings.getSettingsFileName() + ").");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private void printMessage(String message)
+	{
+		Calendar cal = Calendar.getInstance();
+		String dateAndTime = dateFormat.format(cal.getTime());
+
+		message = "["+dateAndTime+"] " + message;
+		
+		System.out.println(message);
+		
+		if(RaspberrySchedulerSettings.isPrintToLog())
+		{
+			appendToLog(message);
+		}
+	}
+	
+	private void appendToLog(String message)
+	{
+		try {
+		    Files.write(Paths.get(RaspberrySchedulerSettings.getLogFileName()), message.getBytes(), StandardOpenOption.APPEND);
+		}catch (IOException e) {
+		}
+	}
+	
 }
--- src/raspberryScheduler/RaspberrySchedulerSettings.java
+++ src/raspberryScheduler/RaspberrySchedulerSettings.java
@@ -3,12 +3,28 @@ package raspberryScheduler;
 public class RaspberrySchedulerSettings {
 
 	private final static boolean debug = true;
+	private final static boolean printToLog = true;
 	
-	private final static String SETTINGS = "RaspberryScheduler.ini";
-	private final static String LOG = "RaspberrySchedulerLog.ini";
+	private final static String SETTINGS_FILE_NAME = "RaspberryScheduler.ini";
+	private final static String LOG_FILE_NAME = "RaspberrySchedulerLog.ini";
 	
 	public static boolean isDebug()
 	{
 		return debug;
+	}
+	
+	public static String getSettingsFileName()
+	{
+		return SETTINGS_FILE_NAME;
+	}
+	
+	public static String getLogFileName()
+	{
+		return LOG_FILE_NAME;
+	}
+	
+	public static boolean isPrintToLog()
+	{
+		return printToLog;
 	}
 }
