a5a364664d3a4b2a5cc5f9dca4bcce1853115844
whoodini <popa.n.marius@gmail.com>
2018-01-30 01:15:27 +0100

    meh?

--- Marius/4_14_health_potion.py
+++ Marius/4_14_health_potion.py
@@ -1,15 +1,32 @@
+#health potion
 import random
+import math
 
-health = 50
-diff=3
+#initial.health
+initial_health = 50
+
+#difficulty
+difficulty_level = 2
+
+#smtg
 #low=int(25*diff)
 #high=int(50*diff)
-potion_health = int(random.randint(25, 50)/diff)
-print("health",health)
-print("diff",diff)
-#print("low",low)
-#print("high",high)
-print("potion_health",potion_health)
 
-health = health + potion_health
-print ("health value", health)
+#random number generator
+random_number = random.randint(25, 50)
+#potion heath
+potion_health = int ( random_number / difficulty_level )
+
+#print suff
+print ("health =",initial_health)
+print ("difficulty level =",difficulty_level)
+#print ("low",low)
+#print ("high",high)
+print ("random number generator =", random_number)
+print ("potion health =",potion_health)
+
+current_health = initial_health + potion_health
+print ("curent health value =", current_health)
+
+#unknown
+#print(math.floor())
---
+++ Marius/5_21_hello_you.py
@@ -0,0 +1,24 @@
+# Ask user for name
+print("="*1)
+name = input("What is your name?: ")
+
+# Ask user for thei age
+print("="*1)
+age = input("What is your age?: ")
+
+# Ask user for their city
+print("="*1)
+city = input("What is your city?: ")
+
+# Ask user what they enjoy
+print("="*1)
+love = input ("What do you love doing?: ")
+
+# Create output text
+print("="*1)
+string = "Your name is {} and you are {} years old. You live in {} and you love {}"
+output = string.format(name, age, city, love)
+
+# Print output to screen
+print("="*40)
+print (output)
