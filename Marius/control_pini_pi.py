98190a7e056afc155c04cc02b9515b0c028bdada
whoodini <35895002+whoodini@users.noreply.github.com>
2018-04-29 13:46:44 +0200

    new one
    new one

--- Marius/control_pini_pi.py
+++ Marius/control_pini_pi.py
@@ -12,9 +12,10 @@ def setPinDown(pin_id):
     print(str(pin_id) + " is DOWN")
 
 def readDataFromfile():
+    global pin_settings
+
     with open('pinSettings.txt') as json_file:  
         pin_settings = json.load(json_file)
-        print(type(pin_settings))
         '''
         pin = '0'
         for times in pin_settings[pin]:
@@ -22,40 +23,10 @@ def readDataFromfile():
             print('StopTime: ' + times['StopTime'])
             print('')
         '''
-
 def saveDataToFile():
     with open('pinSettingsX.txt', 'w') as json_file:  
         json.dump(pin_settings, json_file, indent=4)
-'''
-functie care printeaza timpi pinilor start/stop
-compare date-time
-'''
-'''
-#create pin initial data
-pin_settings = {}  
-pin_settings['0'] = []  
-pin_settings['0'].append({  
-    'StartTime': '16:20',
-    'StopTime': '16:30'
-})
-pin_settings['0'].append({  
-    'StartTime': '20:00',
-    'StopTime': '21:00'
-})
-pin_settings['1'] = []  
-pin_settings['1'].append({  
-    'StartTime': '16:40',
-    'StopTime': '16:50'
-})
-pin_settings['2'] = []  
-pin_settings['2'].append({  
-    'StartTime': '15:20',
-    'StopTime': '16:30'
-})
 
-with open('pinSettings.txt', 'w') as outfile:  
-    json.dump(pin_settings, outfile, indent=4)
-'''
 #start main program
 print("\n"*20)
 
@@ -63,28 +34,47 @@ count = 0
 
 readDataFromfile()
 
-while True:
-    print("\n" + str(datetime.now().time()))
-    
-    """
-    Pin_Id
-    [Timp_start, Timp_stop]
-    """
-    setPinUP(count)
-    time.sleep(1)
-    setPinDown(count)
-    #pin_settings[count].append({'StartTime : {}'.format(str(datetime.now().time()),)})
-    #pin_settings[count].append('is it working?')
-    #print(pin_setting)
-    print(type(pin_settings))
+print("\n" + str(datetime.now().strftime("%H:%M")))
+        
+#setPinUP(count)
+#time.sleep(1)
+#setPinDown(count)
 
-    #exit condition
-    if count == 2:
-        print("\nAm ajuns la:", count)
-        #print(pin_settings(count))
-        #print(pin_settings())
+now = datetime.now().time()
+nowInMinutes = now.hour * 60 + now.minute
 
-        #saveDataToFile()
-        break
-    else:
-        count = count + 1
+for key in pin_settings.keys():
+    print("\nWorking with pin no: " + key)
+    for orar in pin_settings[key]:
+        for t in orar:
+            print(t," :", orar[t])
+            split = orar[t].split(":")
+            then = int(split[0]) * 60 + int(split[1])
+            
+            if nowInMinutes > then:
+                if t == 'StartTime':
+                    print("nowInMinutes: ", nowInMinutes, "is > then: ", then)
+                    print("Your {}: {} is smaller than {}.".format(t,orar[t],datetime.now().strftime("%H:%M")))
+                    setPinUP(key)
+                    #print("setPinDown({})".format(key))
+                if t == 'StopTime':
+                    print("nowInMinutes: ", nowInMinutes, "is > then: ", then)
+                    print("Your {}: {} is smaller than {}.".format(t,orar[t],datetime.now().strftime("%H:%M")))
+                    setPinDown(key)
+                    #print("setPinDown({})".format(key))
+
+            elif nowInMinutes < then:
+                if t == 'StartTime':
+                    print("nowInMinutes: ", nowInMinutes, "is < then: ", then)
+                    print("Your {}: {} is greater than {}.".format(t,orar[t],datetime.now().strftime("%H:%M")))
+                    #setPinUP(key)
+                    #print("setPinDown({})".format(key))
+                if t == 'StopTime':
+                    print("nowInMinutes: ", nowInMinutes, "is < then: ", then)
+                    print("Your {}: {} is greater than {}.".format(t,orar[t],datetime.now().strftime("%H:%M")))
+                    setPinDown(key)
+                    #print("setPinDown({})".format(key))
+
+            else:
+                print("nowInMinutes: ", nowInMinutes, "is = then: ", then)
+                print("Your {}: {} is equal than {}.".format(t,orar[t],datetime.now().strftime("%H:%M")))
