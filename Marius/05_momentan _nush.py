1a1a09b508600d01d8cc65e537466fb1b8fa8a34
whoodini <popa.n.marius@gmail.com>
2018-05-25 12:46:26 +0200

    new from laptop
    new from laptop

--- Marius/05_momentan _nush.py
+++ Marius/05_momentan _nush.py
@@ -1,6 +1,8 @@
 from random import randint
 
-def unique_id(self):
+baza_de_date = {}
+
+def unique_id():
     identificator_unic = randint(0,1000)
     return identificator_unic
 
@@ -8,15 +10,13 @@ class Client:
     def __init__(self, nume, prenume):
         self.first_name = nume
         self.last_name = prenume
-        #self.baza_de_date = {}
     
     def adaugaClient(self, nume, prenume):
-        self.baza_de_date[unique_id] = Client(nume, prenume)
+        baza_de_date[unique_id()] = {}
     
     def listeazaClientii(self):
-        for unique_id in self.baza_de_date:
-            print(unique_id)
-            print("ID: {}, Nume: {}, Prenume: {}".format(unique_id, self.first_name, self.last_name))
+        for any_key in baza_de_date:
+            print("ID: {}, Nume: {}, Prenume: {}".format(any_key, self.first_name, self.last_name))
 
 class Produs(Client):
     def __init__(self, produs, greutate, defect):
@@ -24,12 +24,12 @@ class Produs(Client):
         self.weight = greutate
         self.defekt = defect
 
-baza_de_date = {}
-
 # main prog
 client_nou = Client("Mike", "Kowalsky")
 produs_nou = Produs("Handy", 350, "ecran spart")
 
 # baza_de_date.listeazaClientii()
-baza_de_date.adaugaClient(client_nou)
-baza_de_date.listeazaClientii()
+# baza_de_date.adaugaClient()
+baza_de_date.adaugaClient("a","b")
+client_nou.listeazaClientii()
+print(baza_de_date)
