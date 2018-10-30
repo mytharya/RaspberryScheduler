c0ef9c21e1309e4bf5bd8caccc63ee2d2155cd88
whoodini <popa.n.marius@gmail.com>
2018-10-30 18:17:42 +0100

    n
    n

--- Marius/w3resources/w3_py_basic_p1_ex-16.py
+++ Marius/w3resources/w3_py_basic_p1_ex-16.py
@@ -1,20 +1,30 @@
 # w3resources-16
 # Write a Python program to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference.
 
+values = {'first':None, 'second':None}
+
 def ask4input(place):
     while True:
         try:
+            global value
             value = int(input('Enter {} poizitive numeric value: '.format(place)))
             break
         except:
             print('NO NO NO sir! thats not descifrable')
 
-values = {'first':None, 'second':None}
-
 for key in values:
-    print(key)
     ask4input(key)
+    values[key] = value
 
-# ask4input(first)
-# ask4input(second)
-print('meh!4')
+def all_keys_in_dict():
+    keyz = list(values.keys())
+    # print(keyz)
+
+all_keys_in_dict
+print(values(keyz(0)))
+
+# def compare_values():
+#     for key in values:
+#         values[keyz[0]] <
+
+print('meh!')
