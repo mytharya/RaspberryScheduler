64e785caad4fcfb3dd95d32f39930f0e30ded8d5
whoodini <popa.n.marius@gmail.com>
2018-11-08 11:04:09 +0100

    new&new
    new&new

--- Marius/w3resources/w3_py_basic_p1_ex-18.py
+++ Marius/w3resources/w3_py_basic_p1_ex-18.py
@@ -24,14 +24,48 @@ def make_a_list_of_indexes_from_a_dict(dict_name):
     keyz = list(values.keys())
 
 def compare_values_of_dict():
-    if keyz[0] == keyz[1] == keyz[2]:
-        return (keyz[0] + keyz[1] + keyz[2])*3
+    if values[keyz[0]] == values[keyz[1]] == values[keyz[2]]:
+        return (values[keyz[0]] + values[keyz[1]] + values[keyz[2]])*3
     else:
-        return keyz[0] + keyz[1] + keyz[2]
-    
-place_holder()
-print(keyz)
-# print(compare_values_of_dict())
+        return values[keyz[0]] + values[keyz[1]] + values[keyz[2]]
 
-print(values)
+print values.keys()
+print values.values()
+print values.items()
+
+# print(len(values))
+# print(len(values.keys()))
+
+# print(values(key))
+# for len(values[key]) in values:
+#     print(key[])
+#     if 0 < key and key < len(values):
+#         print(key)
+
+
+
+place_holder()
+# print(type(values.keys()))
+make_a_list_of_indexes_from_a_dict(values)
+print(compare_values_of_dict())
+
+# print('_'*60)
+# print(keyz[0])
+# print(values.keys())
+
+# print(len(values.keys()))
+
+# kk = 0
+# if kk < len(values.keys()):
+
+# while kk < len(values.keys()):
+#     print(kk)
+#     kk += 1
+#     print(values[keyz[0]])
+
+
+# for keyxx in values.keys():
+#     print(keyxx)
+
+
 print('wait! is it k?')
---
+++ Marius/w3resources/w3_py_basic_p1_ex-19.py
@@ -0,0 +1,21 @@
+# w3resources-18
+# Write a Python program to get a new string from a given string where "Is" has been added to the front.
+# If the given string already begins with "Is" then return the string unchanged.
+
+def ask4input():
+    while True:
+        try:
+            global value
+            value = str(input('Give me some text: '))
+            break
+        except:
+            print("oh noes! noes vowels for yous!")
+
+ask4input()
+if 'Is' in value:
+    print ('There is not "Is" in!')
+else:
+    new_value = 'Is ' + value
+
+
+print(new_value)
