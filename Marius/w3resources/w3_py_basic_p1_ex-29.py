eaf4a85a04ca42d1f3b963c2cb5e11dbafef3142
whoodini <35895002+whoodini@users.noreply.github.com>
2018-12-06 23:55:05 +0100

    Update w3_py_basic_p1_ex-29.py

--- Marius/w3resources/w3_py_basic_p1_ex-29.py
+++ Marius/w3resources/w3_py_basic_p1_ex-29.py
@@ -1,20 +1,15 @@
 # w3resources-29
 # Write a Python program to print out a set containing all the colors from color_list_1 which are not present in color_list_2.
 # Test Data : 
-# color_list_1 = set(["White", "Black", "Red"]) 
-# color_list_2 = set(["Red", "Green"])
+color_list_1 = set(["White", "Black", "Red"]) 
+color_list_2 = set(["Red", "Green"])
 # Expected Output : 
 # {'Black', 'White'}
 
-# color_list_3 = color_list_1.difference(color_list_2)
-# print(color_list_3)
+color_list_3 = color_list_2.difference(color_list_1)
+color_list_4 = color_list_1.difference(color_list_2)
 
-import timeit
-
-# color1 = temp1 = list(range(100))
-# color2 = [i * 2 for i in range(50)]
-# init = temp1 = list(range(100)); temp2 = [i * 2 for i in range(50)]
-temp1 = list(range(99))
-temp2 = [i * 2 for i in range(50)]
-
-print (timeit.timeit(temp2.difference(temp1)))
+print("Your 1st list contains {} colors. ".format(color_list_1))
+print("Your 2st list contains {} colors. ".format(color_list_2))
+print("The missing colors in 1st list are: {}.".format(color_list_3))
+print("The missing colors in 2nd list are: {}.".format(color_list_4))
